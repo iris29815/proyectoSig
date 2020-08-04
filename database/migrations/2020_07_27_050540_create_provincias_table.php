610d9c867dec547c36d705e902c7f8f9568beffb
iris29815 <iris.serrudo.7@gmail.com>
2020-08-04 16:23:05 -0400

    primer que estoy subiendo mis cambios

---
+++ database/migrations/2020_07_27_050540_create_provincias_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProvinciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provincias', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->enum('movilidad', ['flota', 'buses','surubi', 'micro', 'mini']);
+            $table->decimal('latitud',10,8);
+            $table->decimal('longitud',10,8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('provincias');
+    }
+}
---
+++ resources/views/usuarios.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <usuario-component></usuario-component>
+</div>
+@endsection
